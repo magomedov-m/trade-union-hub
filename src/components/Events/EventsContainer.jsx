12014d2dae538d5d8b459535cc85bd75679808b5
magomedov-m <magomedov.dgu@gmail.com>
2025-06-08 00:11:25 +0300

    реализовал отправку отзывов в бд

--- src/components/Events/EventsContainer.jsx
+++ src/components/Events/EventsContainer.jsx
@@ -1,28 +1,16 @@
 import React from 'react'
 import styles from './EventsContainer.module.scss'
-import Image from 'next/image'
-import medVenatus from '../../../public/medVenatus.jpg'
-import Goryanka from '../../../public/goryanka.jpg'
-import StreetBall from '../../../public/streetball.jpg'
+// import Image from 'next/image'
+// import medVenatus from '../../../public/medVenatus.jpg'
+// import Goryanka from '../../../public/goryanka.jpg'
+// import StreetBall from '../../../public/streetball.jpg'
+import { Button } from '@mui/material'
+import Link from 'next/link'
 
 export default function EventsContainer() {
   return (
     <div className={styles.content}>
-        <div className={styles.eventsContainer}>
-            <div className={`${styles.eventsItem} ${styles.eventsTitle}`}>
-            <h1>Актуальные события</h1>
-            <p className={styles.p}>Узнайте о предстоящих мероприятиях</p>
-        </div>
-        <div className={styles.eventsItem}>
-            <Image className={styles.img} src={medVenatus} alt='мероприятие' />                       
-        </div>
-        <div className={styles.eventsItem}>
-            <Image className={styles.img} src={StreetBall} alt='мероприятие' />
-        </div>
-        <div className={styles.eventsItem}>
-            <Image className={styles.img} src={Goryanka} alt='мероприятие' />
-        </div>
-        </div>
+        <Link href='/about'><Button>Написать отзыв</Button></Link>
     </div>
   )
 }
