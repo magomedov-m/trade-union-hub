66e6ea38e2972149bc9d47ed5d502fdb245cbcb9
magomedov-m <magomedov.dgu@gmail.com>
2025-06-12 15:15:47 +0300

    Сделал отображение отзывов по одобрению админом

--- src/components/FeedbackVeiw/FeedbackView.jsx
+++ src/components/FeedbackVeiw/FeedbackView.jsx
@@ -27,18 +27,30 @@ export default function FeedbackView() {
       setError("Непредвиденная ошибка при загрузке:", error);
     }
   }
+
+  async function update(id, isApproved) {
+    const { error } = await supabase
+      .from("feedback_message")
+      .update({ is_approved: !isApproved })
+      .eq("id", id);
+      
+  }
   return (
     <div className={styles.content}>
-      {!feedback.length ? Array.from({ length: 10 }, (_, index) => <Skeletone key={index} />) : feedback.map((item, idx) => {
-        return <div key={idx} className={styles.reviewCard}>
-          <h6>{`${item.created_at}`.slice(0, 19)}</h6>
-          <p>
-            “{item.text}”
-          </p>
-          <h4>{`${item.last_name} ${item.first_name}`}</h4>
-          <Button variant="outlined">{item.is_approved ? 'Скрыть' : 'Одобрить'}</Button>
-        </div>;
-      })}
+      {!feedback.length
+        ? Array.from({ length: 10 }, (_, index) => <Skeletone key={index} />)
+        : feedback.map((item, idx) => {
+            return (
+              <div key={idx} className={styles.reviewCard}>
+                <h6>{`${item.created_at}`.slice(0, 19)}</h6>
+                <p>“{item.text}”</p>
+                <h4>{`${item.last_name} ${item.first_name}`}</h4>
+                <Button onClick={() => update(item.id, item.is_approved)} variant="outlined">
+                  {item.is_approved ? "Скрыть" : "Одобрить"}
+                </Button>
+              </div>
+            );
+          })}
     </div>
   );
 }
